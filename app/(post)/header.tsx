fad8d850cf93f6f458d6b6c1a1a0f08d48208f8b
shabi <96938160+shabi@users.noreply.github.com>
2026-07-11 20:37:42 +0800

    Update header.tsx

--- app/(post)/header.tsx
+++ app/(post)/header.tsx
@@ -38,11 +38,11 @@ export function Header({ posts }: { posts: Post[] }) {
           <span className="hidden md:inline">
             <span>
               <a
-                href="https://twitter.com/rauchg"
+                href="mailto:admin@ohhoba.com"
                 className="hover:text-neutral-800 dark:hover:text-neutral-400"
                 target="_blank"
               >
-                @rauchg
+                @ohhoba
               </a>
             </span>
 
