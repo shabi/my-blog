c4fb6699726272cc21946e146bfd181e157fb191
shabi <96938160+shabi@users.noreply.github.com>
2026-07-11 23:53:13 +0800

    Update route.tsx

--- app/about/opengraph-image/route.tsx
+++ app/about/opengraph-image/route.tsx
@@ -8,7 +8,7 @@ import commaNumber from "comma-number";
 
 // Image
 const rauchgPhoto = toArrayBuffer(
-  readFileSync(join(process.cwd(), "public/images/rauchg.png"))
+  readFileSync(join(process.cwd(), "public/images/gang.png"))
 );
 
 // Fonts
