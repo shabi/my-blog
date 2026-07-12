d3db7eb9205743e64b6bec9fe7a5f420b59b792a
shabi <96938160+shabi@users.noreply.github.com>
2026-07-12 19:11:28 +0800

    Update Atom feed details for OhHoBa

--- app/atom/route.ts
+++ app/atom/route.ts
@@ -6,15 +6,15 @@ export async function GET() {
   return new Response(
     `<?xml version="1.0" encoding="utf-8"?>
   <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>Guillermo Rauch</title>
+    <title>OhHoba</title>
     <subtitle>Essays</subtitle>
-    <link href="https://rauchg.com/atom" rel="self"/>
-    <link href="https://rauchg.com/"/>
+    <link href="https://ohhoba.com/atom" rel="self"/>
+    <link href="https://ohhoba.com/"/>
     <updated>${posts[0].date}</updated>
-    <id>https://rauchg.com/</id>
+    <id>https://ohhoba.com/</id>
     <author>
-      <name>Guillermo Rauch</name>
-      <email>rauchg@gmail.com</email>
+      <name>OhHoBa</name>
+      <email>admin@ohhoba.com</email>
     </author>
     ${posts.slice(0, max).reduce((acc, post) => {
       const dateMatch = post.date.match(/\d{4}/);
@@ -23,7 +23,7 @@ export async function GET() {
         <entry>
           <id>${post.id}</id>
           <title>${post.title}</title>
-          <link href="https://rauchg.com/${dateMatch[0]}/${post.id}"/>
+          <link href="https://ohhoba.com/${dateMatch[0]}/${post.id}"/>
           <updated>${post.date}</updated>
         </entry>`;
     }, "")}
