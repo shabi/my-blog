6b3c9219afd745180fda0b08688959e371c20796
shabi <96938160+shabi@users.noreply.github.com>
2026-07-13 21:10:12 +0800

    Create page.tsx

---
+++ app/zh/page.tsx
@@ -0,0 +1,10 @@
+import { Posts } from "../posts";
+import { getPosts } from "../get-posts";
+
+export const revalidate = 300;
+
+export default async function Home() {
+  const posts = await getPosts("zh");
+
+  return <Posts posts={posts} />;
+}
