98d96a4848899b3f83d6a8dff07a64b00373626f
shabi <96938160+shabi@users.noreply.github.com>
2026-07-13 22:22:24 +0800

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -1,44 +1,68 @@
+"use client";
+
 import { Logo } from "./logo";
 import Link from "next/link";
 import { Mail } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export function Header() {
+  const pathname = usePathname();
+
+  const isChinese = pathname.startsWith("/zh");
+
   return (
-    <header className="
-    sticky
-    top-0
-    z-50
-    flex
-    items-center
-    py-4
-    -mx-6
-    px-6
-    bg-[#fcfcfc]/80
-    dark:bg-[#1C1C1C]/80
-    backdrop-blur
-	">
-		
+    <header
+      className="
+        sticky
+        top-0
+        z-50
+        flex
+        items-center
+        py-4
+        -mx-6
+        px-6
+        bg-[#fcfcfc]/80
+        dark:bg-[#1C1C1C]/80
+        backdrop-blur
+      "
+    >
       <Logo />
 
       <nav className="text-xs grow justify-end items-center flex">
+
         <Link
-          href="/about"
+          href={isChinese ? "/" : "/zh"}
           className="group p-2"
         >
-	  <span className="group-hover:bg-neutral-200 dark:group-hover:bg-neutral-700 rounded-xl py-0.5 px-1.5 inline-flex">
-		  About
-	  </span>
+          <span className="group-hover:bg-neutral-200 dark:group-hover:bg-neutral-700 rounded-xl py-0.5 px-1.5 inline-flex">
+            {isChinese ? "EN" : "中"}
+          </span>
         </Link>
+
+
+        <Link
+          href={isChinese ? "/zh/about" : "/about"}
+          className="group p-2"
+        >
+          <span className="group-hover:bg-neutral-200 dark:group-hover:bg-neutral-700 rounded-xl py-0.5 px-1.5 inline-flex">
+            {isChinese ? "关于" : "About"}
+          </span>
+        </Link>
+
+
         <a
           href="mailto:admin@ohhoba.com"
           target="_blank"
           className="group inline-flex items-center p-2 rounded-sm transition-[background-color] whitespace-nowrap -mr-2"
         >
-	  <span className="group-hover:bg-neutral-200 dark:group-hover:bg-neutral-700 rounded-xl py-0.5 px-1.5 inline-flex items-center gap-1">
-	    <EmailIcon style={{ marginRight: 4 }} />
-	    <span>Contact me</span>
-	  </span>
+          <span className="group-hover:bg-neutral-200 dark:group-hover:bg-neutral-700 rounded-xl py-0.5 px-1.5 inline-flex items-center gap-1">
+            <EmailIcon style={{ marginRight: 4 }} />
+            <span>
+              {isChinese ? "联系我" : "Contact me"}
+            </span>
+          </span>
         </a>
+
       </nav>
     </header>
   );
