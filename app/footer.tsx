a5de928ea290cde77f404b346249dae7b859eef7
shabi <96938160+shabi@users.noreply.github.com>
2026-07-13 23:54:37 +0800

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { A } from "./(post)/components/a";
 
 export function Footer() {
+  const pathname = usePathname();
+
+  const isChinese = pathname.startsWith("/zh");
+
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
@@ -10,9 +17,13 @@ export function Footer() {
         </A>
         )
       </div>
+
       <div>
-        <A target="_blank" href="https://github.com/rauchg/blog">
-          Source
+        <A
+          target="_blank"
+          href="https://github.com/rauchg/blog"
+        >
+          {isChinese ? "源码" : "Source"}
         </A>
       </div>
     </footer>
