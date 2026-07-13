53c560c9677a5aaa92a6eaaef048515460d913a8
shabi <96938160+shabi@users.noreply.github.com>
2026-07-13 23:50:25 +0800

    Update logo.tsx

--- app/logo.tsx
+++ app/logo.tsx
@@ -1,16 +1,27 @@
 "use client";
+
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export function Logo() {
   const pathname = usePathname();
+
+  const isChinese = pathname.startsWith("/zh");
+
+  const homePath = isChinese ? "/zh" : "/";
+
+  const isHome =
+    pathname === "/" || pathname === "/zh";
+
   return (
     <span className="text-md md:text-lg whitespace-nowrap font-bold">
-      {pathname === "/" ? (
-        <span className="cursor-default pr-2">OhHoBa</span>
+      {isHome ? (
+        <span className="cursor-default pr-2">
+          OhHoBa
+        </span>
       ) : (
         <Link
-          href="/"
+          href={homePath}
           className="hover:bg-neutral-200 dark:hover:bg-neutral-700 p-2 rounded-2xl -ml-2 transition-[background-color]"
         >
           OhHoBa
