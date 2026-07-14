2162a330905927fb2a5829d3e3316e4479767c32
shabi <96938160+shabi@users.noreply.github.com>
2026-07-14 08:54:11 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { Posts } from "./posts";
 import { getPosts } from "./get-posts";
+import { ScrollTop } from "./scroll-top";
 
 export const revalidate = 300;
 
 export default async function Home() {
   const posts = await getPosts();
-  return <Posts posts={posts} lang="en" />;
+
+  return (
+    <>
+      <ScrollTop />
+      <Posts posts={posts} />
+    </>
+  );
 }
