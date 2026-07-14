63d9c217bdca3ce1cae1d5093bc1d9114c167bdc
shabi <96938160+shabi@users.noreply.github.com>
2026-07-14 09:29:37 +0800

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -10,6 +10,8 @@ export function Footer() {
 
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+      <div className="grow" />
+
       <div>
         <A
           target="_blank"
