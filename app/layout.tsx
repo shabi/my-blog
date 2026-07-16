64db4083b94253d537eaaa94d67c94fa55c0954a
shabi <96938160+shabi@users.noreply.github.com>
2026-07-16 11:22:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,7 +85,7 @@ export default function RootLayout({
     >
       <body className="dark:text-gray-100 max-w-2xl m-auto">
         <main className="p-6 pt-3 md:pt-6 min-h-screen">
-          <Header lang="en" />
+          <Header />
           {children}
         </main>
 
