2db1cb5e957a5172584ac0afbbe35dd5d8389816
shabi <96938160+shabi@users.noreply.github.com>
2026-07-17 16:35:51 +0800

    Create layout.tsx

---
+++ app/about/layout.tsx
@@ -0,0 +1,11 @@
+{
+  "@context": "https://schema.org",
+  "@type": "AboutPage",
+  "name": "About OhHoBa",
+  "url": "https://ohhoba.com/about",
+  "about": {
+    "@type": "Organization",
+    "name": "OhHoBa",
+    "url": "https://ohhoba.com"
+  }
+}
