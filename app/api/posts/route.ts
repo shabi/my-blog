94c98566bbf68fd245d29b566071f226c2e57717
shabi <96938160+shabi@users.noreply.github.com>
2026-07-31 10:37:31 +0800

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import { getPosts } from "../../get-posts";
 
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
 
-  const lang = searchParams.get("lang") ?? "en";
+export async function GET() {
 
-  return NextResponse.json(await getPosts(lang));
+  return NextResponse.json(await getPosts());
+
 }
